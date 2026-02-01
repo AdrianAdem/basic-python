43f31014eb036dcc877987c66f7b9a57f7cd2fef
Adrian12762 <ademovic0@web.de>
2026-02-01 17:08:49 +0100

    indoor.py

--- 01_functions,variables/calculator.py
+++ 01_functions,variables/calculator.py
@@ -1,6 +1,10 @@
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+def main():
+    # Get user input
+    x = int(input("Enter first number: "))
+    print("x squared is:", square(x))
+    
+# Define the function
+def square(n):
+    return n * n
 
-z = round(x + y)
-
-print(z)
+main()
